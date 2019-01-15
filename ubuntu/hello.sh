2af568a7eb8a50a348783a3104a1bf2f3d65f658
Saurabh Kaushal <kaushal.saurabh@gmail.com>
2019-01-14 20:56:25 -0800

    First shell script

---
+++ ubuntu/hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Echo this is my first comment
+echo "Hello world this is my first bash script!"
+echo "Iam executing the script with user:"
+whoami
+echo "I am currently running in directory:"
+pwd
+exit 0
